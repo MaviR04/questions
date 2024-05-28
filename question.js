a644c1ca8b363c5fd7ecd7009ee3e627233e5510
Mavi <mavirodrigo4@gmail.com>
2024-05-28 16:54:15 +0530

    Added Modules, new AI modules

--- question.js
+++ question.js
@@ -17,6 +17,16 @@ const questions={
     
 }
 
+const questions2={
+    0: "Define Artificial Intelligence (AI) and its primary goals.",
+    1: "Explain how Machine Learning (ML) differs from traditional programming.",
+    2: "Describe the main focus of Deep Learning (DL) and how it extends Machine Learning.",
+    3:'Name some real World applications of AI',
+    4:"Name some technologies used in self driving cars",
+    5:"What is the machine learning process",
+    6:"explain the essential steps in preprocessing "
+}
+
 const answers={
 1: "RAM slots on a motherboard provide sockets for installing RAM (Random Access Memory) modules. RAM is used by the CPU to store data and instructions that are actively being used during program execution.",
 2: "The chipset acts as a communication hub between various hardware components such as the CPU, RAM, GPU, and peripheral devices. It manages data flow and controls the interaction between these components and the CPU.",
@@ -35,14 +45,42 @@ const answers={
 15: "Examples of protocols used in each layer of the OSI model include Ethernet for the Data Link layer, IP for the Network layer, TCP and UDP for the Transport layer, HTTP and FTP for the Application layer, and many others."
 }
 
-let i=0;
-const qno = Object.keys(questions);
-qno.sort(function(){return 0.5-Math.random()}) 
-console.log(qno)
+const answers2={
+    0: "AI is the simulation of human intelligence processes by machines, primarily computer systems. Its primary goals include reasoning, learning, perception, problem-solving, and language understanding.",
+    1: "In traditional programming, explicit instructions are provided to perform tasks. In contrast, ML algorithms learn from data to improve performance over time without being explicitly programmed.",
+    2: "DL focuses on using deep neural networks with many layers to model and understand complex patterns in data. It extends ML by automatically learning hierarchical features from raw data, enabling more accurate and efficient representation learning.",
+    3:'Virtual Personal Assistants(eg:siri,alexa)\nAutonomous Vehicles(tesla)\nCRT systems(tiktok,youtube)',
+    4:"Sensors to measure distance\nVision/Image Processing\nDeep learning for decision making",
+    5:'Define the problem\nGather data\nPreprocessing\nFeature engineering (just a part of pre-processing) : normalization, categorical data -> numeric, new feature extraction, handling quality (outliers)\nSplit data: training, validation (tune) and testing (evaluate performance)\nHyper parameter tuning (parameters set apart from data set. Eg: learning rate, number of hidden layers of NN, depth of decision tree)\nEvaluation\nDeployment\nMonitor and maintain',
+    6:'Data cleaning\nData Intergration\nData Transformation\nData Reduction'
+}
 
-function qgetter(displayer){
-    if(flag==true){
-        displayer(questions,qno[i],"questionbox");
+let i=0;
+let flag=true
+
+  //this function puts all the questoin numbers in a list abd shuffles the order randomly
+function sorter(mod){    
+    if (flag==true){                             //this if condition exists to make sure the shuffle only happens one time
+    const qno = Object.keys(mod);
+    qno.sort(function(){return 0.5-Math.random()}); 
+    console.log(qno);                      
+    flag=false                   
+    return qno
+    }
+}
+
+
+
+
+let mod ;
+let anscript;
+
+
+
+
+function qgetter(displayer,mod){
+    {
+        displayer(mod,qno[i],"questionbox");
         console.log(i)
         if (i<qno.length){
             i++
@@ -51,24 +89,11 @@ function qgetter(displayer){
             document.getElementById("questionbox").innerText = 'Congrats!, you finished the module';
             document.getElementById("show").style = 'display:unset;'
     }}
-    flag=false
-}
-
-let flag = true
-let correctamount = 0
-
-
-function adder(){
-    if (i<=qno.length && flag==false){
-        correctamount++
-        flag=true
-        console.log('Correct: '+correctamount)
-    }
-    else{
-        flag=true
-    }
 
 }
+
+
+
 
 function showresults(){
     document.getElementById('result').innerText = `You got ${correctamount} out of ${qno.length}`
@@ -79,3 +104,4 @@ function showresults(){
  function display(obj,no,id){  
     document.getElementById(id).innerText = obj[no];
  }
+
